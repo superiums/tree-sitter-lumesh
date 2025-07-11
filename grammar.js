b1becf75cf2cd8ce18ef6a8b0414141642681872
superiums <superium@126.com>
2025-07-11 18:56:35 +0800

    highlight could see

--- grammar.js
+++ grammar.js
@@ -9,62 +9,60 @@
 
 module.exports = grammar({
   name: "lumesh",
-
+  word: ($) => $.symbol,
   extras: ($) => [/\s/, $.comment],
 
-  // conflicts: ($) => [[$.primary_expression, $.command_expression]],
+  // conflicts: ($) => [[$.range_expr, $.float]],
 
   rules: {
-    source_file: ($) => repeat($._statement),
+    lumesh: ($) => repeat($.statements),
 
-    _statement: ($) =>
-      choice(
-        $.expression_statement,
-        $.declaration,
-        $.assignment,
-        $.control_flow,
-        $.function_definition,
-        $.use_statement,
-        $.alias_statement,
-        $.del_statement,
+    statements: ($) =>
+      seq(
+        choice(
+          $.expression,
+          $.declaration,
+          $.assignment,
+          $.control_flow,
+          $.function_def,
+          $.use_statement,
+          $.alias_statement,
+          $.del_statement,
+        ),
+        optional(choice(";", "\n")),
       ),
-
-    expression_statement: ($) => seq($.expression, optional(choice(";", "\n"))),
 
     // 表达式 - 按优先级从低到高排列
     expression: ($) =>
       choice(
-        $.assignment_expression, // PREC_ASSIGN = 1
-        $.pipe_expression, // PREC_PIPE = 2
-        $.catch_expression, // PREC_CATCH = 3
-        $.lambda_expression, // PREC_LAMBDA = 4
-        $.conditional_expression, // PREC_CONDITIONAL = 5
-        $.logical_or_expression, // PREC_LOGICAL_OR = 6
-        $.logical_and_expression, // PREC_LOGICAL_AND = 7
-        $.comparison_expression, // PREC_COMPARISON = 8
-        $.additive_expression, // PREC_ADD_SUB = 11
-        $.multiplicative_expression, // PREC_MUL_DIV = 12
-        $.power_expression, // PREC_POWER = 13
-        $.custom_expression, // PREC_CUSTOM = 14
-        $.unary_expression, // PREC_UNARY = 20, PREC_PREFIX = 21
-        $.postfix_expression, // PREC_INDEX = 25
-        $.primary_expression, // PREC_LITERAL = 29 ==>39
-        $.command_expression, // PREC_CMD_ARG = 9
+        $.group,
+        $.assign_expr, // PREC_ASSIGN = 1
+        $.pipe_expr, // PREC_PIPE = 2
+        $.catch_expr, // PREC_CATCH = 3
+        $.lambda_expr, // PREC_LAMBDA = 4
+        $.conditional_expr, // PREC_CONDITIONAL = 5
+        $.logical_or_expr, // PREC_LOGICAL_OR = 6
+        $.logical_and_expr, // PREC_LOGICAL_AND = 7
+        $.comparison_expr, // PREC_COMPARISON = 8
+        $.add_sub_expr, // PREC_ADD_SUB = 11
+        $.mul_div_expr, // PREC_MUL_DIV = 12
+        $.power_expr, // PREC_POWER = 13
+        $.custom_expr, // PREC_CUSTOM = 14
+        $.unary_expr, // PREC_UNARY = 20, PREC_PREFIX = 21
+        $.postfix_expr, // PREC_INDEX = 25
+        $.primary_expr, // PREC = 29 ==>39
+        $.command_expr, // PREC_CMD_ARG = 9
       ),
 
     // 赋值表达式 (优先级 1, 右结合)
-    assignment_expression: ($) =>
+    assign_expr: ($) =>
       prec.right(
         1,
-        seq(
-          $.identifier,
-          choice("=", ":=", "+=", "-=", "*=", "/="),
-          $.expression,
-        ),
+        seq($.symbol, choice("=", ":=", "+=", "-=", "*=", "/="), $.expression),
       ),
 
     // 管道表达式 (优先级 2, 左结合)
-    pipe_expression: ($) =>
+    pipe_expr: ($) =>
       prec.left(
         2,
         seq(
@@ -75,7 +73,7 @@ module.exports = grammar({
       ),
 
     // 错误处理表达式 (优先级 3, 左结合)
-    catch_expression: ($) =>
+    catch_expr: ($) =>
       prec.left(
         3,
         choice(
@@ -85,30 +83,29 @@ module.exports = grammar({
       ),
 
     // Lambda 表达式 (优先级 4, 右结合)
-    lambda_expression: ($) =>
+    lambda_expr: ($) =>
       prec.right(
         4,
         seq(
-          choice($.identifier, seq("(", optional(commaSep1($.parameter)), ")")),
+          choice($.symbol, seq("(", optional(commaSep1($.parameter)), ")")),
           "->",
           choice($.expression, $.block),
         ),
       ),
 
     // 条件表达式 (优先级 5, 右结合)
-    conditional_expression: ($) =>
+    conditional_expr: ($) =>
       prec.right(5, seq($.expression, "?", $.expression, ":", $.expression)),
 
     // 逻辑或 (优先级 6, 左结合)
-    logical_or_expression: ($) =>
-      prec.left(6, seq($.expression, "||", $.expression)),
+    logical_or_expr: ($) => prec.left(6, seq($.expression, "||", $.expression)),
 
     // 逻辑与 (优先级 7, 左结合)
-    logical_and_expression: ($) =>
+    logical_and_expr: ($) =>
       prec.left(7, seq($.expression, "&&", $.expression)),
 
     // 比较表达式 (优先级 8, 左结合)
-    comparison_expression: ($) =>
+    comparison_expr: ($) =>
       prec.left(
         8,
         seq(
@@ -131,49 +128,49 @@ module.exports = grammar({
       ),
 
     // 命令参数表达式 (优先级 9, 左结合)
-    command_expression: ($) =>
+    command_expr: ($) =>
       prec.left(
         9,
         seq(
-          $.identifier,
+          field("command", $.symbol),
           repeat1(
             choice(
-              $.identifier,
-              $.string_literal,
+              $.symbol,
+              $.string,
               $.string_raw,
-              $.argument_symbol,
-              $.integer_literal,
-              $.float_literal,
+              $.argument,
+              $.integer,
+              $.float,
             ),
           ),
+          optional(choice("&", "&-", "&+", "&?", "&.")),
         ),
       ),
 
     // 加减表达式 (优先级 11, 左结合)
-    additive_expression: ($) =>
+    add_sub_expr: ($) =>
       prec.left(11, seq($.expression, choice("+", "-"), $.expression)),
 
     // 乘除模表达式 (优先级 12, 左结合)
-    multiplicative_expression: ($) =>
+    mul_div_expr: ($) =>
       prec.left(12, seq($.expression, choice("*", "/", "%"), $.expression)),
 
     // 幂运算 (优先级 13, 右结合)
-    power_expression: ($) =>
-      prec.right(13, seq($.expression, "^", $.expression)),
+    power_expr: ($) => prec.right(13, seq($.expression, "^", $.expression)),
 
     // 自定义操作符 (优先级 14, 左结合)
-    custom_expression: ($) =>
+    custom_expr: ($) =>
       prec.left(
         14,
         seq(
           $.expression,
-          /_.*/, // 以 _ 开头的自定义操作符
+          field("custom_op", /_.*/), // 以 _ 开头的自定义操作符
           $.expression,
         ),
       ),
 
     // 一元表达式 (优先级 20-21)
-    unary_expression: ($) =>
+    unary_expr: ($) =>
       choice(
         prec.left(20, seq(choice("!", "-"), $.expression)),
         prec.left(21, seq(choice("++", "--"), $.expression)),
@@ -181,22 +178,30 @@ module.exports = grammar({
       ),
 
     // 后缀表达式 (优先级 25)
-    postfix_expression: ($) =>
+    postfix_expr: ($) =>
       choice(
         $.function_call, //26
-        $.index_expression, //29
-        $.slice_expression, //30
-        $.chain_expression, //27
-        // $.property_expression,  //28
-        $.range_expression, //25
+        $.index_expr, //29
+        $.slice_expr, //30
+        $.chain_expr, //27
+        // $.property_expr,  //28
+        $.range_expr, //25
       ),
 
     // 函数调用 (优先级 25)
     function_call: ($) =>
-      prec(26, seq($.expression, "(", optional(commaSep1($.expression)), ")")),
+      prec(
+        26,
+        seq(
+          field("name", $.symbol),
+          "(",
+          optional(commaSep1($.expression)),
+          ")",
+        ),
+      ),
 
     // 索引表达式 (优先级 25)
-    index_expression: ($) =>
+    index_expr: ($) =>
       prec.left(
         29,
         seq(
@@ -204,14 +209,14 @@ module.exports = grammar({
           choice(
             seq("[", $.expression, "]"),
             seq("@", $.expression),
-            seq(".", $.expression),
+            // seq(".", $.expression),
           ),
         ),
       ),
 
     // 切片表达式 (优先级 25)
-    slice_expression: ($) =>
-      prec(
+    slice_expr: ($) =>
+      prec.left(
         30,
         seq(
           $.expression,
@@ -225,66 +230,66 @@ module.exports = grammar({
       ),
 
     // 链式调用表达式 (优先级 25)
-    chain_expression: ($) =>
-      prec(
+    chain_expr: ($) =>
+      prec.left(
         27,
         seq(
           $.expression,
           repeat1(
-            seq(".", $.identifier, "(", optional(commaSep1($.expression)), ")"),
+            seq(
+              ".",
+              field("method", $.symbol),
+              "(",
+              optional(commaSep1($.expression)),
+              ")",
+            ),
           ),
         ),
       ),
 
     // 属性访问表达式 (优先级 25) - 分离出来
-    // property_expression: ($) => prec(25, seq($.expression, ".", $.identifier)),
+    // property_expr: ($) => prec(25, seq($.expression, ".", $.symbol)),
 
     // 范围表达式 (优先级 25)
-    range_expression: ($) =>
+    range_expr: ($) =>
       prec.left(
         25,
         seq(
-          $.expression,
+          choice($.integer, $.symbol, $.variable),
           choice("..", "...", "...<", "..<"),
-          $.expression,
-          optional(seq(":", $.expression)),
+          choice($.integer, $.symbol, $.variable),
+          optional(seq(":", choice($.integer, $.symbol, $.variable))),
         ),
       ),
 
     // 基础表达式 (优先级 39)
-    primary_expression: ($) =>
-      prec(
-        39,
-        choice($.identifier, $.variable, $.literal, $.list, $.map, $.group),
-      ),
+    primary_expr: ($) =>
+      prec.left(39, choice($.symbol, $.variable, $.literal, $.list, $.map)),
 
     // 字面量
     literal: ($) =>
       choice(
-        $.integer_literal,
-        $.float_literal,
-        $.string_literal,
+        $.integer,
+        $.float,
+        $.string,
         $.string_raw,
         $.string_template,
-        $.boolean_literal,
-        $.none_literal,
+        $.boolean,
+        $.none,
       ),
 
-    integer_literal: ($) =>
+    integer: ($) =>
       token(seq(optional("-"), choice("0", seq(/[1-9]/, repeat(/[0-9]/))))),
 
-    float_literal: ($) =>
+    float: ($) =>
       token(
         seq(
           optional("-"),
-          choice(
-            seq(/[0-9]+/, ".", repeat(/[0-9]/)),
-            seq("0", ".", repeat(/[0-9]/)),
-          ),
+          choice(seq(/[0-9]+/, ".", /[0-9]+/), seq("0", ".", /[0-9]+/)),
         ),
       ),
 
-    string_literal: ($) =>
+    string: ($) =>
       token(
         seq(
           '"',
@@ -322,30 +327,30 @@ module.exports = grammar({
         ),
       ),
 
-    boolean_literal: ($) => choice("True", "False"),
-    none_literal: ($) => "None",
+    boolean: ($) => choice("True", "False"),
+    none: ($) => "None",
 
     // 标识符和变量
-    identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
-    variable: ($) => seq("$", $.identifier),
+    symbol: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
+    variable: ($) => seq("$", $.symbol),
 
     // 集合类型
-    list: ($) => seq("[", optional(commaSep1($.expression)), "]"),
+    list: ($) => prec(37, seq("[", optional(commaSep1($.expression)), "]")),
 
-    map: ($) => prec(49, seq("{", optional(commaSep1($.map_entry)), "}")),
+    map: ($) => prec(37, seq("{", optional(commaSep1($.map_entry)), "}")),
 
     map_entry: ($) =>
-      seq(choice($.identifier, $.string_literal), ":", $.expression),
+      seq(field("key", choice($.symbol, $.string)), ":", $.expression),
 
     // 分组
-    group: ($) => prec(60, seq("(", $.expression, ")")),
+    group: ($) => prec(38, seq("(", $.expression, ")")),
 
     // 参数定义
     parameter: ($) =>
-      prec(9, seq($.identifier, optional(seq("=", $.expression)))),
+      prec(9, seq(field("name", $.symbol), optional(seq("=", $.expression)))),
 
     // 命令参数符号
-    argument_symbol: ($) =>
+    argument: ($) =>
       token(
         choice(
           seq("--", /[a-zA-Z][a-zA-Z0-9-]*/),
@@ -369,78 +374,117 @@ module.exports = grammar({
     declaration: ($) =>
       seq(
         "let",
-        choice(seq($.identifier, "=", $.expression), $.destructure_assignment),
+        choice(
+          seq($.symbol, "=", $.expression),
+          $.multi_assign,
+          $.destruct_list,
+          $.destruct_map,
+        ),
       ),
 
-    destructure_assignment: ($) =>
-      prec(2, seq(commaSep1($.identifier), "=", $.expression)),
+    multi_assign: ($) =>
+      prec(1, seq(commaSep1($.symbol), "=", commaSep1($.expression))),
+    destruct_list: ($) =>
+      prec(
+        1,
+        seq(
+          "[",
+          commaSep1($.symbol),
+          "]",
+          "=",
+          choice($.symbol, $.variable, $.list),
+        ),
+      ),
+    destruct_map: ($) =>
+      prec(
+        1,
+        seq(
+          "{",
+          commaSep1($.symbol, optional(seq(":", $.symbol))),
+          "}",
+          "=",
+          choice($.symbol, $.variable, $.map),
+        ),
+      ),
 
     assignment: ($) =>
-      seq(
-        $.identifier,
-        choice("=", ":=", "+=", "-=", "*=", "/="),
-        $.expression,
-      ),
+      seq($.symbol, choice("=", ":=", "+=", "-=", "*=", "/="), $.expression),
 
     // 控制流
     control_flow: ($) =>
       choice(
-        $.if_expression,
-        $.while_expression,
-        $.for_expression,
-        $.loop_expression,
-        $.match_expression,
+        $.if_expr,
+        $.while_expr,
+        $.for_expr,
+        $.loop_expr,
+        $.match_expr,
         $.return_statement,
         $.break_statement,
       ),
 
-    if_expression: ($) =>
+    if_expr: ($) =>
       seq(
         "if",
-        $.expression,
+        field("condition", $.expression),
         $.block,
-        optional(seq("else", choice($.block, $.if_expression))),
+        optional(seq("else", choice($.block, $.if_expr))),
       ),
 
-    while_expression: ($) => seq("while", $.expression, $.block),
+    while_expr: ($) => seq("while", field("condition", $.expression), $.block),
 
-    for_expression: ($) =>
-      seq("for", $.identifier, "in", $.expression, $.block),
+    for_expr: ($) =>
+      seq(
+        "for",
+        field("variable", $.symbol),
+        "in",
+        field("iterable", $.expression),
+        $.block,
+      ),
 
-    loop_expression: ($) => seq("loop", $.block),
+    loop_expr: ($) => seq("loop", $.block),
 
-    match_expression: ($) =>
-      seq("match", $.expression, "{", repeat($.match_arm), "}"),
+    match_expr: ($) =>
+      seq("match", field("value", $.expression), "{", repeat($.match_arm), "}"),
 
     match_arm: ($) =>
-      seq(commaSep1($.expression), "=>", $.expression, optional(",")),
+      seq(
+        choice("_", commaSep1($.expression)),
+        "=>",
+        $.expression,
+        choice(";", "\n"),
+      ),
 
     return_statement: ($) => prec.right(seq("return", optional($.expression))),
     break_statement: ($) => prec.right(seq("break", optional($.expression))),
 
     // 函数定义
-    function_definition: ($) =>
+    function_def: ($) =>
       seq(
         "fn",
-        $.identifier,
+        field("name", $.symbol),
         "(",
         optional(
-          seq(commaSep1($.parameter), optional(seq(",", "...", $.identifier))),
+          seq(commaSep1($.parameter), optional(seq(",", "...", $.symbol))),
         ),
         ")",
         $.block,
       ),
 
     // 代码块
-    block: ($) => prec(50, seq("{", repeat($._statement), "}")),
+    block: ($) => prec(36, seq("{", repeat($.statements), "}")),
 
     // 其他语句
     use_statement: ($) =>
-      seq("use", $.string_literal, optional(seq("as", $.identifier))),
+      seq(
+        "use",
+        field("module", $.string),
+        optional(seq("as", field("alias", $.symbol))),
+      ),
 
-    alias_statement: ($) => seq("alias", $.identifier, "=", $.expression),
+    alias_statement: ($) =>
+      seq("alias", field("name", $.symbol), "=", $.expression),
 
-    del_statement: ($) => seq("del", $.identifier),
+    del_statement: ($) => seq("del", $.symbol),
 
     // 注释
     comment: ($) => token(seq("#", /.*/)),
@@ -452,6 +496,6 @@ function commaSep1(rule) {
   return seq(rule, repeat(seq(",", rule)));
 }
 
-function commaSep(rule) {
-  return optional(commaSep1(rule));
-}
+// function commaSep(rule) {
+//   return optional(commaSep1(rule));
+// }
--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,44 +1,174 @@
-; 关键字  
-[  
-  "if" "else" "fn" "let" "for" "while" "match"  
-  "return" "break" "loop" "use" "del" "alias"  
-  "in"  
-] @keyword  
-  
-; 操作符  
-[  
-  "+" "-" "*" "/" "%" "^"  
-  "==" "!=" ">" "<" ">=" "<="  
-  "&&" "||" "!"   
-  "=" "+=" "-=" "*=" "/=" ":="  
-  "|" "|>" "<<" ">>" ">>!" "|_" "|^"  
-  "->" ".." "..." "...<" "..<"  
-  "~~" "~=" "~:" "!~~" "!~:"  
-  "?." "?+" "??" "?>" "?!" "?:"  
-] @operator  
-  
-; 字符串  
-(string_literal) @string  
-(string_raw) @string.special  
-(string_template) @string.regexp  
-  
-; 数字  
-(integer_literal) @number  
-(float_literal) @number.float  
-  
-; 注释  
-(comment) @comment  
-  
-; 标识符  
-(identifier) @variable  
-(variable) @variable.parameter  
-  
-; 函数  
-  
-; 特殊值  
-(none_literal) @constant.builtin  
-(boolean_literal) @constant.builtin  
-  
-; 标点符号  
-["(" ")" "[" "]" "{" "}"] @punctuation.bracket  
-["," ";" ":"] @punctuation.delimiter
+; Keywords
+[
+  "let"
+  "fn"
+  "if"
+  "else"
+  "while"
+  "for"
+  "in"
+  "loop"
+  "match"
+  "return"
+  "break"
+  "use"
+  "as"
+  "alias"
+  "del"
+] @keyword
+
+; Operators
+[
+  "="
+  ":="
+  "+="
+  "-="
+  "*="
+  "/="
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "^"
+  "!"
+  "++"
+  "--"
+  "=="
+  "!="
+  ">"
+  "<"
+  ">="
+  "<="
+  "~~"
+  "~="
+  "~:"
+  "!~~"
+  "!~:"
+  "&&"
+  "||"
+  "|"
+  "|>"
+  "<<"
+  ">>"
+  ">>!"
+  "|_"
+  "|^"
+  "?."
+  "?+"
+  "??"
+  "?>"
+  "?!"
+  "?:"
+  "?"
+  ":"
+  "->"
+  "=>"
+] @operator
+
+; Custom operators (starting with _)
+;(custom_expr
+;  custom_op: (_) @operator)
+
+; Punctuation
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  ","
+  ";"
+  "."
+  "@"
+] @punctuation.delimiter
+
+; Literals
+(integer) @number
+(float) @number
+(string) @string
+(string_raw) @string
+(string_template) @string
+(boolean) @boolean
+(none) @constant.builtin
+
+; Variables and symbols
+(variable) @variable
+(symbol) @variable
+
+; Function definitions and calls
+;(function_def
+;  name: (symbol) @function)
+
+(function_call
+  name: (symbol) @function)
+
+(chain_expr) @_chain
+;(chain_expr
+;  method: (symbol) @function.method)
+
+; Function parameters
+(parameter
+  name: (symbol) @variable.parameter)
+
+; Control flow
+(if_expr
+  condition: (expression) @variable)
+
+(while_expr
+  condition: (expression) @variable)
+
+(for_expr
+  variable: (symbol) @variable.parameter
+  iterable: (expression) @variable)
+
+(match_expr
+  value: (expression) @variable)
+
+; Comments
+(comment) @comment
+
+; Command expressions
+(command_expr
+  command: (symbol) @function)
+
+(argument) @string.special
+
+; Map entries
+(map_entry
+  key: (symbol) @property)
+
+(map_entry
+  key: (string) @property)
+
+; Use statements
+(use_statement
+  module: (string) @string)
+
+(use_statement
+  alias: (symbol) @variable)
+
+; Alias statements
+(alias_statement
+  name: (symbol) @variable)
+
+; Range operators
+[
+  ".."
+  "..."
+  "...<"
+  "..<"
+] @operator
+
+; Output control
+[
+  "&"
+  "&-"
+  "&+"
+  "&."
+  "&?"
+] @operator
